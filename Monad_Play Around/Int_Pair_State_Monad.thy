332fb2ee0ad84f9152ff64288a53517e1679e994
Simon Thrane <sthranehansen@gmail.com>
2020-03-26 11:58:59 +0100

    Add folder with Monads
    Added a folder with different experiments with monads

---
+++ Monad_Play Around/Int_Pair_State_Monad.thy
@@ -0,0 +1,185 @@
+theory Int_Pair_State_Monad
+imports 
+  Main
+  "~~/src/HOL/Library/State_Monad"
+begin
+
+type_synonym 'a pair = "'a \<times> 'a"
+
+type_synonym 'a env = "'a \<times> 'a"
+
+
+definition return:: "'a \<Rightarrow> ('b, 'a) state" where "return = State_Monad.return"
+definition get:: "('a pair, 'a pair) state" where "get = State (\<lambda>x. (x,x))"
+definition put:: "'a pair \<Rightarrow> ('a pair, unit) state" where "put x = State (\<lambda>_. ((),x))"
+definition get_fst:: "('a pair, 'a) state" where "get_fst = do { x \<leftarrow> get; return (fst x) }" 
+definition get_snd:: "('a pair, 'a) state" where "get_snd = do { x \<leftarrow> get; return (snd x) }" 
+definition set_fst:: "'a pair \<Rightarrow> 'a \<Rightarrow> 'a pair" where "set_fst v x = (x, snd v)"
+definition set_snd:: "'a pair \<Rightarrow> 'a \<Rightarrow> 'a pair" where "set_snd v x = (fst v, x)"
+definition put_fst:: "'a \<Rightarrow> ('a pair, unit) state" where "put_fst x = do { v \<leftarrow> get; put (set_fst v x) }"
+definition put_snd:: "'a \<Rightarrow> ('a pair, unit) state" where "put_snd x = do { v \<leftarrow> get; put (set_snd v x) }"
+definition skip:: "('a pair, unit) state" where "skip = State (\<lambda>x. ((),x))"
+
+
+definition inject_npu:: "(nat pair, unit) state" where "inject_npu = State (\<lambda>x. ((),x))"
+
+fun monfact:: "nat \<Rightarrow> (nat pair, unit) state" where
+mf1: "monfact 0 = skip" |
+mf2: "monfact (Suc n) = do { y \<leftarrow> get_fst; y \<leftarrow> return (y * (Suc n)); put_fst y; monfact n }"
+
+fun fibacc :: "nat \<Rightarrow> nat => nat \<Rightarrow> nat" where
+  "fibacc 0 a b = a"
+| "fibacc (Suc 0) a b = b"
+| "fibacc (Suc (Suc n)) a b = fibacc (Suc n) b (a+b)"
+
+value \<open>fibacc 6 0 1 = 8\<close>
+value \<open>fibacc 9 0 1 = 34\<close>
+
+definition fib_wrap:: "nat \<Rightarrow> nat" where
+"fib_wrap n = fibacc n 0 1"
+
+fun fib :: "nat => nat" where
+  "fib 0 = 0"
+| "fib (Suc 0) = 1"
+| "fib (Suc (Suc x)) = fib x + fib (Suc x)"
+
+lemma [simp]: "fib (Suc n) > 0"
+  by (induct n rule: fib.induct) simp_all
+
+text {* Alternative induction rule. *}
+
+theorem fib_induct:
+    "P 0 ==> P 1 ==> (!!n. P (n + 1) ==> P n ==> P (n + 2)) ==> P (n::nat)"
+  by (induct rule: fib.induct) simp_all
+
+lemma fib_wrap: "fib_wrap n = fib n"
+proof (induct n)
+  case 0
+  then show ?case 
+  by (simp add: fib_wrap_def)
+next
+  case (Suc n)
+  fix k
+  have fib(k) = fib_wrap(k)
+  then show ?case 
+    sorry
+qed
+
+
+
+text\<open>The fibonacci function does always return the result at the fst value of the pair. The initial state passed in should be (0,1)\<close>
+fun monfib:: "nat \<Rightarrow> (nat pair, unit) state" where
+  "monfib 0 = skip" |
+  "monfib (Suc 0) = do {a \<leftarrow> get_snd; put_fst a}" |
+  "monfib (Suc (Suc n)) = do { a \<leftarrow> get_fst; b \<leftarrow> get_snd; temp_b \<leftarrow> return b; b \<leftarrow> return (a + b); put (temp_b,b); monfib (Suc n)}"
+
+value \<open>fst(snd(run_state (monfib 4) e))\<close>
+
+lemma fib_add:
+  "fib (n + k + 1) = fib (k + 1) * fib (n + 1) + fib k * fib n"
+  (is "?P n")
+proof (induct n rule: fib_induct)
+  show "?P 0" by simp
+  show "?P 1" by simp
+  fix n
+  have "fib (n + 2 + k + 1)
+    = fib (n + k + 1) + fib (n + 1 + k + 1)" by simp
+  also assume "fib (n + k + 1)
+    = fib (k + 1) * fib (n + 1) + fib k * fib n"
+      (is " _ = ?R1")
+  also assume "fib (n + 1 + k + 1)
+    = fib (k + 1) * fib (n + 1 + 1) + fib k * fib (n + 1)"
+      (is " _ = ?R2")
+  also have "?R1 + ?R2
+    = fib (k + 1) * fib (n + 2 + 1) + fib k * fib (n + 2)"
+    by (simp add: add_mult_distrib2)
+  finally show "?P (n + 2)" .
+qed
+
+
+lemma monfib_aux: "fst(snd (run_state (monfib (Suc (Suc n))) x)) = fst(snd (run_state (monfib n) x)) + fst(snd (run_state (monfib (Suc n)) x))"
+  apply (induction n arbitrary: x)
+   apply (simp add:  skip_def)
+   apply (simp add: get_def set_fst_def)
+  sledgehammer
+
+
+lemma fib_basic: "fst(snd(run_state (monfib n) x)) = fibacc n ((fst x)::nat) ((snd x)::nat)"
+  apply (induction n arbitrary: x)
+   apply (simp add:  skip_def)
+  sorry
+
+value \<open>fst(snd (run_state (monfact 10) e))\<close>
+lemma monfact_mult: "fst(snd (run_state (monfact n) (a * b, y))) =  b * fst(snd (run_state (monfact n) (a, y)))"
+  apply (induction n arbitrary: a b)
+  apply (simp add: skip_def)
+  apply (simp add: return_def put_def get_def)
+  by (metis mult.assoc mult.commute mult_Suc)
+  
+lemma monfact_shift: "fst(snd (run_state (monfact (Suc n)) (x,y))) = fst(snd (run_state (monfact n) ((Suc n) * x, y)))"
+  apply (induction n arbitrary: x y)
+  apply (simp add: get_fst_def put_fst_def return_def)
+   apply (simp add: put_fst_def return_def get_def fst_def snd_def)
+  sorry
+
+lemma monfact_aux: "fst(snd (run_state (monfact (Suc n)) x)) = (Suc n) * fst(snd (run_state (monfact n) x))"
+  apply (induction n arbitrary: x)
+   apply (simp add: skip_def return_def get_def put_def)
+  by (metis monfact_mult monfact_shift mult.commute)
+
+fun accfact:: "nat \<Rightarrow> nat \<Rightarrow> nat" where
+af1:  "accfact 0 a = a" |
+af2: "accfact (Suc n) a = accfact n (a * Suc n)"
+
+lemma accfact_mult: "accfact n (a * b) = b * accfact n a"
+  apply (induction n arbitrary: a b)
+   apply (simp)
+  apply (simp only: af2)
+  by (simp add: semiring_normalization_rules(19))
+
+lemma accfact_aux: "accfact (Suc n) x = (Suc n) * accfact n x"
+  apply (induction n)
+   apply simp
+  using Int_Pair_State_Monad.af2 accfact_mult by presburger
+
+
+value "fst(snd (run_state (monfact 6) (1::nat, y::nat)))"
+value "accfact 6 1"
+
+
+lemma fact_basic: "fst(snd (run_state (monfact n) (x::nat,y::nat))) = accfact n (fst(x::nat,y::nat))"
+  apply (induction n arbitrary: x y)
+   apply (simp add: put_fst_def get_fst_def snd_def fst_def skip_def)
+  sledgehammer
+
+
+
+lemma monad_example1: "fst(snd(run_state (do { put (1, 1) }) (0, 0))) = 1" 
+  by (simp add: put_def)
+
+lemma monad_example2: "snd(snd(run_state (do { put (1, 1) }) (0, 0))) = 1" 
+  by (simp add: put_def)
+
+lemma monad_example3: "fst(run_state (do { put (1, 2); x \<leftarrow> get_fst; return x }) (0, 0)) = 1"
+  by (simp add: get_fst_def put_def get_def return_def)
+
+lemma monad_example4: "fst(run_state (do { put (1, 2); x \<leftarrow> get_snd; return x }) (0, 0)) = 2"
+  by (simp add: get_snd_def put_def get_def return_def)
+
+value "snd(snd((run_state (do { x \<leftarrow> return (3::nat); put_snd x })) (2, 2)))"
+
+lemma monad_example5: "snd(snd((run_state (do { x \<leftarrow> return 1; put_snd x })) (2, 2))) = 1"
+  by (simp add: put_snd_def put_def set_snd_def get_def return_def)
+
+lemma monad_example6: "fst(snd((run_state (do { x \<leftarrow> return 1; put_snd x })) (2, 2))) = 2"
+  by (simp add: put_snd_def put_def set_snd_def get_def return_def)
+
+definition swap:: "(nat pair, unit) state" 
+  where "swap = do { a \<leftarrow> get_fst; b \<leftarrow> get_snd; put_fst b; put_snd a }"
+
+lemma monad_example7: "snd (run_state swap (x,y)) = (y,x)"
+  by (simp add: swap_def put_def get_fst_def get_snd_def put_fst_def put_snd_def set_fst_def set_snd_def get_def return_def)
+
+
+
+end
---
+++ Monad_Play Around/Int_State_Monad.thy
@@ -0,0 +1,126 @@
+theory Int_State_Monad
+imports 
+  Main
+  "~~/src/HOL/Library/State_Monad"
+begin
+
+definition return:: "'a \<Rightarrow> ('b, 'a) state" where "return = State_Monad.return"
+definition get:: "(nat, nat) state" where "get = State (\<lambda>x. (x,x))"
+definition put:: "nat \<Rightarrow> (nat, unit) state" where "put x = State (\<lambda>_. ((),x))"
+definition skip:: "(nat, unit) state" where "skip = State (\<lambda>x. ((),x))"
+
+lemma monad_example1: "fst(run_state (do { return 1 }) 2) = 1" 
+  by (simp add: return_def)
+
+lemma monad_example2: "fst(run_state (do { put 1; x \<leftarrow> get; return x }) 2) = 1"
+  by (simp add: get_def put_def return_def)
+
+lemma monad_example3: "snd(run_state (do { put 1; x \<leftarrow> get; return x }) 2) = 1"
+  by (simp add: get_def put_def return_def)
+
+lemma monad_example4: 
+"fst((run_state (do { x \<leftarrow> return 1; put x; return x })::(nat \<Rightarrow> nat \<times> nat)) 2) = 1"
+  by (simp add: put_def return_def)
+
+lemma monad_example5: "snd((run_state (do { x \<leftarrow> return 1; put x })::(nat \<Rightarrow> unit \<times> nat)) 2) = 1"
+  by (simp add: put_def return_def)
+
+definition prog:: "(nat, nat) state" 
+  where "prog = do { a \<leftarrow> get; b \<leftarrow> return (a + 1); put b; return b }"
+
+definition init_prog:: "(nat, nat) state" where "init_prog = do { put 1; prog }"
+
+lemma monad_example6: "snd (run_state init_prog 0) = 2"
+  by (simp add: init_prog_def prog_def put_def get_def return_def)
+
+fun accfact:: "nat \<Rightarrow> nat \<Rightarrow> nat" where
+af1:  "accfact 0 a = a" |
+af2: "accfact (Suc n) a = accfact n (a * Suc n)"
+
+lemma accfact_mult: "accfact n (a * b) = b * accfact n a"
+  apply (induction n arbitrary: a b)
+   apply (simp)
+  apply (simp only: af2)
+  by (simp add: semiring_normalization_rules(19))
+
+lemma accfact_aux: "accfact (Suc n) x = (Suc n) * accfact n x"
+  apply (induction n)
+   apply simp
+  using Int_State_Monad.af2 accfact_mult by presburger
+  
+fun monfact:: "nat \<Rightarrow> (nat, unit) state" where
+mf1: "monfact 0 = skip" |
+mf2: "monfact (Suc n) = do { y \<leftarrow> get; y \<leftarrow> return (y * (Suc n)); put y; monfact n }"
+
+lemma monfact_mult: "snd (run_state (monfact n) (a * b)) = b * snd (run_state (monfact n) a)"
+  apply (induction n arbitrary: a b)
+  apply (simp add: skip_def)
+  apply (simp add: return_def put_def get_def)
+  by (metis mult.assoc mult.commute mult_Suc)
+  
+lemma monfact_shift: "snd (run_state (monfact (Suc n)) x) = snd (run_state (monfact n) ((Suc n) * x))"
+  apply (induction n arbitrary: x)
+  apply (simp add: get_def put_def return_def)
+  apply (simp add: put_def return_def get_def)
+  by (simp add: mult.commute)
+
+lemma monfact_aux: "snd (run_state (monfact (Suc n)) x) = (Suc n) * snd (run_state (monfact n) x)"
+  apply (induction n arbitrary: x)
+   apply (simp add: skip_def return_def get_def put_def)
+  by (metis monfact_mult monfact_shift mult.commute)
+  
+lemma fact_basic: "snd (run_state (monfact n) (x::nat)) = accfact n (x::nat)"
+  apply (induction n)
+   apply (simp add: put_def fst_def skip_def)
+  using accfact_aux monfact_aux by auto
+
+lemma fact_main: "snd (run_state (do { put (1::nat); monfact n }) (x::nat)) = accfact n (1::nat)"
+  by (simp add: fact_basic put_def)
+
+fun fibacc :: "nat \<Rightarrow> nat => nat \<Rightarrow> nat" where
+fa1: "fibacc 0 a b = a"| 
+fa2: "fibacc (Suc 0) a b = b"| 
+fa3: "fibacc (Suc (Suc n)) a b = fibacc (Suc n) b (a+b)"
+
+definition fib_wrap:: "nat \<Rightarrow> nat" where
+  "fib_wrap n \<equiv> fibacc n 1 1"
+
+fun monfibacc :: "nat \<Rightarrow> nat => (nat, unit) state" where
+m1:  "monfibacc 0 a = skip"| 
+m3:  "monfibacc (Suc n) a  = do{b \<leftarrow> get; put (a + b); monfibacc n b}"
+
+definition monfib_wrap where
+  "monfib_wrap n \<equiv> (monfibacc n 1)"
+
+value \<open>fibacc 9 1 1 = 55\<close>
+value \<open>snd (run_state (monfibacc 9 0) (1::nat)) = 55\<close>
+value \<open>fib_wrap 9 = 55\<close>
+value \<open>snd (run_state (monfib_wrap 9) (1::nat))\<close>
+value \<open>snd (run_state (monfib_wrap 2) (1::nat))\<close>
+value \<open>snd (run_state (monfib_wrap (Suc 0)) (snd (run_state (monfib_wrap 0) 1)))\<close>
+value \<open>snd (run_state (monfib_wrap (Suc (Suc 0))) 1)\<close>
+
+lemma fib_base[simp]: "snd (run_state (monfib_wrap 0) (Suc 0)) = fib_wrap 0"
+  by (simp add: fib_wrap_def monfib_wrap_def put_def skip_def)
+
+lemma fib_wrap_aux[simp]: "fib_wrap (Suc (Suc n)) = fib_wrap n + fib_wrap (Suc n)"
+  apply (induction n)
+   apply (simp add: fib_wrap_def)
+  using Int_State_Monad.fa3  fib_wrap_def
+  sorry
+
+lemma monfib_aux[simp]: "snd (run_state (monfib_wrap (Suc (Suc n))) x) 
+        = snd (run_state (monfib_wrap (Suc n)) (snd (run_state (monfib_wrap n) x)))"
+  apply (induction n arbitrary: x)
+  sledgehammer
+   apply (simp add: put_def snd_def get_def skip_def)
+  sledgehammer
+  sorry
+
+lemma fib_basic: "snd (run_state (monfib_wrap n) 1) = fib_wrap n"
+  apply (induction n)
+   apply (simp)
+  sorry
+    
+
+end
